ce464dfc19779ad7957e27f3e127ea5a4fbc33db
Justin St-Laurent <if-admin@DESKTOP-C07N9R2.localdomain>
2026-04-16 15:03:07 -0400

    Revert "Sidebar Chat History"
    This reverts commit 19ffe24340555e82a04aad7656b75f641c55fddc.

--- init_ai.sh
+++
@@ -1,3 +0,0 @@
-export OLLAMA_HOST="http://localhost:11434"
-export OLLAMA_MODEL="llama3.2:1b"
-export DEBUG="True"
--- test_local_ai.sh
+++
@@ -1,5 +0,0 @@
-curl http://localhost:11434/api/chat -d '{
-  "model": "llama3.2",
-  "messages": [{"role": "user", "content": "Say hello"}],
-  "stream": false
-}'
